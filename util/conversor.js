279f5addae7733c8b0c9ecf4aed321d5273d758b
LEVI ARAGAO DE CASTRO BERNARDO <levibernardo0753@digitalcollege.internal>
2025-10-28 21:15:57 -0300

    Erro do chalk concertado

--- util/conversor.js
+++ util/conversor.js
@@ -1,7 +1,7 @@
 // Funçao para converter bites para mb
-const converterByteToMb = (bytes) => {
+export const converterByteToMb = (bytes) => {
     return (bytes/1024/1024).toFixed(2);
 }
 
 /* exportando o módulo (função de conversão) para utilizar em qualquer outro arquivo */
-module.exports = {converterByteToMb}
+// module.exports = {converterByteToMb}
